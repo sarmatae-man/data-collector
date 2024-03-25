f05dd6c748b502e8c74953b051a90f17c6233c5a
Evgeniy <150557243+sarmatae-man@users.noreply.github.com>
2024-03-25 23:38:46 +0300

    Delete scripts directory

--- scripts/doc_com.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Сделать commit контейнера с образом doc:one
-docker commit $(docker ps -qf "ancestor=doc:one") doc:one
-
-# Сделать commit контейнера с образом doc:two
-#docker commit $(docker ps -qf "ancestor=doc:two") doc:two
